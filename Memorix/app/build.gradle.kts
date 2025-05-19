024155946ec7d48734187b5bd5488a5663eb4608
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-05-19 13:02:15 +0700

    Add activity profile, fragment edit profile, and Basicflashcard study

--- Memorix/app/build.gradle.kts
+++ Memorix/app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation(libs.circleimageview)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
